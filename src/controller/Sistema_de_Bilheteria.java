ae02086fde6b215192b43fb478c8c5693b7a8e5f
Laboratorios <Laboratorios@LAB203-02>
2018-09-14 15:07:25 -0300

    teste1

---
+++ src/controller/Sistema_de_Bilheteria.java
@@ -0,0 +1,15 @@
+package controller;
+
+import java.util.concurrent.Semaphore;
+
+public class Sistema_de_Bilheteria extends Thread{
+	
+	private int pessoas;
+	private Semaphore semafaro;
+	
+	public void vendas_Bilhete(){
+		
+		
+	}
+
+}
---
+++ src/view/Main.java
@@ -0,0 +1,18 @@
+package view;
+
+import java.util.concurrent.Semaphore;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int pessoas = 300;
+		int max_Pessoas = 100;
+		Semaphore semafaro;
+		
+		
+		
+
+	}
+
+}
